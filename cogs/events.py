311a170d3b8d5a7fa0426fb7420afc3c11c55d20
liner <90984207+r-liner@users.noreply.github.com>
2023-04-20 18:55:33 +0300

    Fixed an issue

--- cogs/events.py
+++ cogs/events.py
@@ -1,5 +1,5 @@
 import nextcord
-from nextcord import utils
+from nextcord import utils, User
 from nextcord.ext import commands
 import random
 from random import choice
@@ -17,58 +17,60 @@ class Events(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
-		msg = message.content.lower()
-
-		if message.author == client.user:
+		if message.author.bot == True:
 			return
+		elif message.author == client.user:
+			return
+		else:
+			msg = message.content.lower()
 		
-		if msg.startswith("привет"):
-			await message.channel.send(f'Приветствую, {message.author.mention}, чем могу быть полезен?')
+			if msg.startswith("привет"):
+				await message.channel.send(f'Приветствую, {message.author.mention}, чем могу быть полезен?')
 
-		if msg.startswith("пока"):
-			await message.channel.send(f'До новых встреч, {message.author.mention}, обращайтесь снова!')
+			if msg.startswith("пока"):
+				await message.channel.send(f'До новых встреч, {message.author.mention}, обращайтесь снова!')
 
-		if msg.startswith('бот инфа'):
-			infa_perc = random.randint(0, 101)
-			emb1 = nextcord.Embed(
-				description=f'{message.author.mention}, вероятность составляет {infa_perc}%',
-				colour=0x00ffaa
-				)
-			await message.channel.send(embed=emb1)
+			if msg.startswith('бот инфа'):
+				infa_perc = random.randint(0, 101)
+				emb1 = nextcord.Embed(
+					description=f'{message.author.mention}, вероятность составляет {infa_perc}%',
+					colour=0x00ffaa
+					)
+				await message.channel.send(embed=emb1)
 
-		if msg.startswith('бот кто'):
-			answers = ["оказывается это", "я думаю, это", "я предполагаю, это", "по моему мнению, это"]
-			result = random.choice(answers)
-			emb1 = nextcord.Embed(
-				description=f'{message.author.mention}, {result} {choice(message.guild.members).mention}!',
-				colour=0x00ffaa
-				)
-			await message.channel.send(embed=emb1)
+			if msg.startswith('бот кто'):
+				answers = ["оказывается это", "я думаю, это", "я предполагаю, это", "по моему мнению, это"]
+				result = random.choice(answers)
+				emb1 = nextcord.Embed(
+					description=f'{message.author.mention}, {result} {choice(message.guild.members).mention}!',
+					colour=0x00ffaa
+					)
+				await message.channel.send(embed=emb1)
 
-		if msg.startswith('бот шип'):
-			answers = ["ваша совместимость", "вы подходите друг другу на"]
-			an = random.choice(answers)
-			ship = random.randint(0, 100)
-			embed = nextcord.Embed(
-				description=f'{message.author.mention}, {an} {ship}%!',
-				colour=0x00ffaa
-				)
-			await message.channel.send(embed=embed)
+			if msg.startswith('бот шип'):
+				answers = ["ваша совместимость", "вы подходите друг другу на"]
+				an = random.choice(answers)
+				ship = random.randint(0, 100)
+				embed = nextcord.Embed(
+					description=f'{message.author.mention}, {an} {ship}%!',
+					colour=0x00ffaa
+					)
+				await message.channel.send(embed=embed)
 				
 
-		if msg.startswith('бот шанс'):
-			arr = ["бесспорно", "предрешено", "никаких сомнений", "определённо да", 
-			"можешь быть уверен в этом", "мне кажется - да", "вероятнее всего", "хорошие перспективы",
-			"знаки говорят - да", "да", "пока не ясно, попробуй снова", "спроси позже",
-			"лучше не рассказывать", "сейчас нельзя предсказать", "сконцентрируйся и спроси опять",
-			"даже не думай", "мой ответ - нет", "по моим данным - нет", "перспективы не очень хорошие",
-			"весьма сомнительно"]
-			ball = random.choice(arr)
-			emb1 = nextcord.Embed(
-				description=f'{message.author.mention}, 🎱 {ball}!',
-				colour=0xffffff
-				)
-			await message.channel.send(embed=emb1)
+			if msg.startswith('бот шанс'):
+				arr = ["бесспорно", "предрешено", "никаких сомнений", "определённо да", 
+				"можешь быть уверен в этом", "мне кажется - да", "вероятнее всего", "хорошие перспективы",
+				"знаки говорят - да", "да", "пока не ясно, попробуй снова", "спроси позже",
+				"лучше не рассказывать", "сейчас нельзя предсказать", "сконцентрируйся и спроси опять",
+				"даже не думай", "мой ответ - нет", "по моим данным - нет", "перспективы не очень хорошие",
+				"весьма сомнительно"]
+				ball = random.choice(arr)
+				emb1 = nextcord.Embed(
+					description=f'{message.author.mention}, 🎱 {ball}!',
+					colour=0xffffff
+					)
+				await message.channel.send(embed=emb1)
 
 	@commands.Cog.listener()
 	async def on_member_join(self, member):
