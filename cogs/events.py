4031ce91834bebaccc4c2f94cf7428f527faca9a
liner <90984207+r-liner@users.noreply.github.com>
2023-08-09 23:15:07 +0300

    Translated to English (0.7.4)

--- cogs/events.py
+++ cogs/events.py
@@ -14,15 +14,20 @@ class Events(commands.Cog):
             pass
 
         elif isinstance(err, errors.CommandNotFound):
-            await ctx.send("Команда не найдена!")
-            print(f"Команда не найдена - {ctx.message.author}: {ctx.message.content}")
+            await ctx.send("Command not found!")
+            print(f"Command not found - {ctx.message.author}: {ctx.message.content}")
 
         elif isinstance(err, errors.MissingPermissions):
-            await ctx.send("Недостаточно прав.")
+            await ctx.send("You haven`t permissions.")
 
     @commands.Cog.listener()
     async def on_message(self, message):
         print("{0.guild} - {0.author}: {0.content}".format(message))
+
+        if message.author.bot:
+            return
+        elif message.author == self.client.user:
+            return
 
 def setup(client):
     client.add_cog(Events(client))
--- cogs/info.py
+++ cogs/info.py
@@ -16,29 +16,29 @@ class Info(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         latency = int(round(self.client.latency * 1000, 1))
-        embed = nextcord.Embed(title="Пинг",
-                               description=f"Задержка составляет **{latency} мс**",
+        embed = nextcord.Embed(title="Ping",
+                               description=f"Latency is **{latency} ms**",
                                colour=nextcord.Color.teal())
         await ctx.send(embed=embed)
 
     @commands.command(aliases=['dev'])
     async def developer(self, ctx):
-        embed = nextcord.Embed(title="Разработчик: **liner#9544**",
+        embed = nextcord.Embed(title="Developer: **liner#9544**",
                                description=f"[GitHub](https://github.com/r-liner)",
                                colour=nextcord.Color.teal())
         await ctx.send(embed=embed)
 
     @commands.command()
     async def source(self, ctx):
-        embed = nextcord.Embed(title=f"Исходный код **{self.client.user}**",
+        embed = nextcord.Embed(title=f"Source **{self.client.user}**",
                                description=f"[GitHub](https://github.com/r-liner/discord-bot-ru/tree/master)",
                                colour=nextcord.Color.teal())
         await ctx.send(embed=embed)
 
     @commands.command()
     async def servers(self, ctx):
-        embed = nextcord.Embed(title=f"Серверы",
-                               description=f"Бот работает на **{str(len(self.client.guilds))}** серверах",
+        embed = nextcord.Embed(title=f"Guilds",
+                               description=f"Bot works on **{str(len(self.client.guilds))}** guilds",
                                colour=nextcord.Color.teal())
         await ctx.send(embed=embed)
 
@@ -67,7 +67,7 @@ class Info(commands.Cog):
         embed.add_field(name="Ping", value=int(round(self.client.latency * 1000, 1)))
         embed.add_field(name="Commands", value=len([x.name for x in self.client.commands]))
 
-        await ctx.send("📊 **Статистика**", embed=embed)
+        await ctx.send("📊 **Stats**", embed=embed)
 
 
 def setup(client):
