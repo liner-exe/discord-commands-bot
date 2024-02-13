cb126affc9aea3b75c0438036f73863fceba613c
r-liner <romamolibog@gmail.com>
2024-02-13 23:57:33 +0300

    Slash commands migration (v0.8)

--- cogs/discord.py
+++ cogs/discord.py
@@ -1,28 +1,24 @@
 import nextcord
-from nextcord.ext import commands
+from nextcord.ext import commands, application_checks
 
 
 class Discord(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    # Avatar
-    @commands.command()
-    @commands.guild_only()
-    async def avatar(self, ctx, *, user: nextcord.Member = None):
-        user = user or ctx.author
-        try:
-            if user.avatar or user.guild_avatar:
-                user_avatar = user.avatar.url
-                embed = nextcord.Embed(title=f"Avatar of user **{user.name}**")
-                embed.set_image(url=user_avatar)
-                await ctx.send(embed=embed)
+    @application_checks.guild_only()
+    @nextcord.slash_command()
+    async def avatar(self, interaction, user: nextcord.Member = nextcord.SlashOption(default=None)):
+        user = user or interaction.user
 
-            elif not user.avatar and not user.guild_avatar:
-                return await ctx.send("User has no avatar.")
+        if user.avatar or user.guild_avatar:
+            user_avatar = user.avatar.url
+            embed = nextcord.Embed(title=f"Avatar of user **{user.name}**")
+            embed.set_image(url=user_avatar)
+            await interaction.send(embed=embed)
 
-        except Exception as err:
-            print(err)
+        elif not user.avatar and not user.guild_avatar:
+            return await interaction.send("User has no avatar.")
 
 
 def setup(client):
