772f9ded8ea5116d861a0cc961513d28aa9d3055
Nazar <78618525+Nazar1ky@users.noreply.github.com>
2024-07-17 13:18:02 +0300

    remove comment

--- cogs/utils/github.py
+++ cogs/utils/github.py
@@ -12,7 +12,6 @@ def check_for_updates(version):
     return False
 
 def changelog():
-    # Use text media_type: https://docs.github.com/en/rest/using-the-rest-api/getting-started-with-the-rest-api#media-types
     headers = {
         "Accept": "application/vnd.github.v3.text+json",
     }
