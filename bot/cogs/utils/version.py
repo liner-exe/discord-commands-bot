51903b76c04af824031eeb03222856e41fc89e33
Molibog Roman <90984207+liner-exe@users.noreply.github.com>
2024-10-09 00:42:07 +0300

    added new parameter to function get

--- bot/cogs/utils/version.py
+++ bot/cogs/utils/version.py
@@ -1,3 +1,8 @@
-def get():
-    with open("version.txt", "r", encoding="utf-8") as file:
+def get(path_to_version: str) -> str:
+    """Get a version of an application.
+
+    Returns:
+        str: Version of an application.
+    """
+    with open(f"{path_to_version}/version.txt", "r", encoding="utf-8") as file:
         return file.read()
